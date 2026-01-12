4e0250485af7d8f41000a1e4a25848fccf900f4b
Proton.je <Proton.je@Rudis-MBP-5.localdomain>
2026-01-12 21:36:45 +0000

    Import QuickBooks model to enable table creation

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,5 +2,6 @@
 from app.models.user import User
 from app.models.invoice import Invoice
 from app.models.product import Product
+from app.models.quickbooks import QuickBooksConnection
 
-__all__ = ['User', 'Invoice', 'Product']
+__all__ = ['User', 'Invoice', 'Product', 'QuickBooksConnection']
