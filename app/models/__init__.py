dd07b5d46d67029266b2920e088f871dbba38cd4
Proton.je <Proton.je@Rudis-MBP-5.localdomain>
2026-01-19 09:23:45 +0000

    Register SupplierAccount model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,5 +4,6 @@ from app.models.invoice import Invoice
 from app.models.product import Product
 from app.models.quickbooks import QuickBooksConnection
 from app.models.password_reset import PasswordResetToken
+from app.models.supplier_account import SupplierAccount
 
-__all__ = ['User', 'Invoice', 'Product', 'QuickBooksConnection']
+__all__ = ['User', 'Invoice', 'Product', 'QuickBooksConnection', 'PasswordResetToken', 'SupplierAccount']
